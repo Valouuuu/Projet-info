430ce865136245ff08e7427f31d634a6cfbefdbc
VISSOUZE Valmont <valmont.vissouze@groupe-esigelec.org>
2023-03-23 15:48:09 +0100

    interaction classe joueur + ajout bdd

--- Joueur.py
+++ Joueur.py
@@ -1,7 +1,8 @@
 class Joueur:
-    def __init__(identifiant, mdp, age) :
+    def __init__(self, identifiant, mdp, age) :
         self.id =  identifiant
         self.mdp = mdp
-        #self.age = age (7 ans min si on a le temps)
+        self.age = age 
+        #7 ans min si on a le temps
         self.win = 0
         self.lose = 0
--- main.py
+++ main.py
@@ -1,2 +1,38 @@
-print("hello world")
-#test2
+#accéder à la base de données
+from mysql.connector import connect
+bdd = connect(host="root", user="root", password="root",database="Velonimo")
+
+#importer la classe
+from joueur import Joueur
+
+
+id = input("id :")
+mdp = input("mdp :")
+age = input("age :")
+joueur = Joueur(id, mdp, age)
+
+
+#accéder et modifier bdd
+# cursor = bdd.cursor()
+# cursor.execute("#ce que on veux faire")
+cursor = bdd.cursor()
+
+
+#Ajout d'un joueur
+
+sql = "INSERT INTO Joueur(jou_user, jou_mdp, jou_age, jou_win, jou_lose) VALUES (%s, %s, %s, %s, %s)"
+cursor.execute(sql, (joueur.id, joueur.mdp, joueur.age, joueur.win, joueur.lose))
+
+
+
+
+#récupérer ensemble des lignes : resultat = cursor.fetchall()
+#parcourir resultat :
+# for Joueur in resultat:
+#     id, mdp = Joueur
+#     print(id, mdp)
+
+
+# mettre en commentaire : Ctrl+K+C
+# enlever commentaire : Ctrl+K+U
+
