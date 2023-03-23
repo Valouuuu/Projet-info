b940252fc660d9bb245ca484ee9c4c70b086e55d
VISSOUZE Valmont <valmont.vissouze@groupe-esigelec.org>
2023-03-23 14:48:54 +0100

    création de la classe joueur

--- Joueur.py
+++ Joueur.py
@@ -1,1 +1,7 @@
-#test
+class Joueur:
+    def __init__(identifiant, mdp, age) :
+        self.id =  identifiant
+        self.mdp = mdp
+        #self.age = age (7 ans min si on a le temps)
+        self.win = 0
+        self.lose = 0
