7c61595439a357ea0981eb6ff7b83911f6eba032
VISSOUZE Valmont <valmont.vissouze@groupe-esigelec.org>
2023-03-28 15:15:51 +0200

    mini chgmt

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Projet-info
 faire limitation age 7 ans minimum si on a le temps
+lien tuto utiliser pygame : https://qq.readthedocs.io/en/latest/
--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 #accéder à la base de données
 from mysql.connector import connect
-bdd = connect(host="127.0.0.1", user="root", password="root",database="velonimo")
+bdd = connect(host="127.0.0.1", user="root", password="root",database="Velonimo")
 
 #importer la classe
 from joueur import Joueur
 
-def creer_un_compte():
-    id = input("id :")
-    mdp = input("mdp :")
-    age = input("age :")
-    joueur = Joueur(id, mdp, age)
-    sql = "INSERT INTO Joueur(jou_user, jou_mdp, jou_age, jou_win, jou_lose) VALUES (%s, %s, %s, %s, %s)"
-    cursor.execute(sql, (joueur.id, joueur.mdp, joueur.age, joueur.win, joueur.lose))
-    bdd.commit()
+
 
 
 #accéder et modifier bdd
@@ -21,6 +14,14 @@ def creer_un_compte():
 
 cursor = bdd.cursor()
 
+# def creer_un_compte():
+id = input("id :")
+mdp = input("mdp :")
+age = input("age :")
+joueur = Joueur(id, mdp, age)
+sql = "INSERT INTO Joueur(jou_user, jou_mdp, jou_age, jou_win, jou_lose) VALUES (%s, %s, %s, %s, %s)"
+cursor.execute(sql, (joueur.id, joueur.mdp, joueur.age, joueur.win, joueur.lose))
+bdd.commit()
 
 #Ajout d'un joueur
 
