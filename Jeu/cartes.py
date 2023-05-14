cf8582309a078661a5e5fbe996cbe31ca4cfb631
Alissouuuu <alissa.dugue26@gmail.com>
2023-05-14 11:25:06 +0200

    on peut afficher main joueur + dos de cartes

--- Jeu/cartes.py
+++ Jeu/cartes.py
@@ -1,8 +1,7 @@
 from mysql.connector import connect
 import pygame
 import random
-from player import Joueur
-import player as p
+import fonctions as f 
 
 
 # class Cartes():
@@ -63,18 +62,7 @@ import player as p
             
 #             return result
 
-
-
-
-
-
-
-
-
-
-
-
-     # def value_card(self, deck):
+ # def value_card(self, deck):
 
     #     final_list = []
         
@@ -92,81 +80,102 @@ import player as p
 
 class Cartes():
     
-    def __init__(self,couleur,valeur):
+    def __init__(self,couleur,valeur): # On créé les cartes avec les paramètres couleur, valeur et le code qui permet d'afficher l'image de la carte 
+        
         pygame.init()
         self.couleur = couleur
         self.valeur = valeur
-        self.image = pygame.image.load('Jeu/images_cartes/card_'+ valeur + '_' + couleur +'.png').convert()
+        self.image = pygame.image.load('Jeu/images_cartes/card_'+ valeur + '_' + couleur +'.png').convert() 
 
 
 
-def creer_cartes(couleur):
-        
-    liste_des_cartes = []
-    j=20
+def creer_cartes(couleur,nb_cartes, liste_des_cartes): #Ici on créer les cartes d'une même couleur, on prend en paramètre la couleur qu'on veut créer et la liste dans laquelle se trouvera toutes les cartes 
     
-    if couleur == 'baroudeur'  :
-        for k in range (1,7):
-            
-            liste_des_cartes.append(Cartes(couleur,str(j + 5)))
-
-    else : 
-        for i in range (1,8):
-            
-            liste_des_cartes.append(Cartes(couleur,str(i)))
+    for i in range (1, nb_cartes + 1): # on fait une boucle pour ajouter les objets cartes dans une liste qui contiendra toutes les cartes 
         
-        return liste_des_cartes
+        liste_des_cartes.append(Cartes(couleur,str(i))) 
+        
+    return liste_des_cartes # Retourne la liste des cartes de la couleur demandée au nombre demandé
  
-def afficher_deck(window,deck_list,j,x,y):
-    deck_display = deck_list[j-1]
-    a=0
-    for i in deck_display:
-        window.blit(i.image,(x + 100*a, y))   
-
-def all_cards():
+ 
+def create_all_cards(): # Ici la fonction permet de créer toutes les cartes du jeu et de les stocker dans la liste all_cards
     
     all_cards = []
-    all_cards.append(creer_cartes('baroudeur'))
-    all_cards.append(creer_cartes('red'))
-    all_cards.append(creer_cartes('blue'))
-    all_cards.append(creer_cartes('green'))
-    all_cards.append(creer_cartes('yellow'))
-    all_cards.append(creer_cartes('brown'))
-    all_cards.append(creer_cartes('pink'))
-    all_cards.append(creer_cartes('grey'))
+    all_cards.append(creer_cartes('baroudeur',6,all_cards))
+    del(all_cards[len(all_cards) - 1])
+    all_cards.append(creer_cartes('red',7,all_cards))
+    del(all_cards[len(all_cards) - 1])
+    all_cards.append(creer_cartes('blue',7,all_cards))
+    del(all_cards[len(all_cards) - 1])
+    all_cards.append(creer_cartes('green',7,all_cards))
+    del(all_cards[len(all_cards) - 1])
+    all_cards.append(creer_cartes('yellow',7,all_cards))
+    del(all_cards[len(all_cards) - 1])
+    all_cards.append(creer_cartes('brown',7,all_cards))
+    del(all_cards[len(all_cards) - 1])
+    all_cards.append(creer_cartes('pink',7,all_cards))
+    del(all_cards[len(all_cards) - 1])
+    all_cards.append(creer_cartes('grey',7,all_cards))
+    del(all_cards[len(all_cards) - 1])
     
 
-    print(all_cards)
-    return all_cards
-    
+    return all_cards # Retourne la liste des cartes 
 
-def init_deck(): 
-    
-    # liste des cartes
-    
-    cartes = all_cards()
-    
-    # liste contenant les mains des joueurs
-    
-    player_deck = []
 
-    # On mélange la liste des cartes pour mélanger les cartes
+def init_deck(cartes): # Cette fonction permet d'innitialiser les main des joueur de manière aléatoire
 
-    random.shuffle(cartes)   
+    player_deck = [] # liste contenant les mains des joueurs
+    
+    random.shuffle(cartes) # On mélange la liste des cartes pour mélanger les cartes
 
-    # On distribue les cartes 
-
-    for i in range (4): 
-        deck = []
-        for j in range (11):
-            deck.append(cartes[j])
-            del(cartes[j])   
-        player_deck.append(deck)
+    for i in range (4): # On distribue les cartes pour le nombre de joueur souhaité, ici 4
+        deck = [] # On créer une liste qui sera la main du joueur 
+        for j in range (11): # On fait une boucle correpondant au nombre de cartes par main, ici 11
+            deck.append(cartes[j]) # On ajoute le permier élément de la liste des cartes qui a été mélangée
+            del(cartes[j]) # On supprime de la liste des cartes la carte qu'on vient d'ajouter 
+        player_deck.append(deck) # une fois la boucle finie, on ajoute la main à la liste des mains des joueurs
 
     
-    return player_deck
+    return player_deck # On retourne la liste complète des main des joueurs 
 
 
+def afficher_deck(window,deck_list,j,x,y): # Ici on veut afficher la main du joueur (avec des coordonnées pour placer la main dans l'écran)
+    # On a pris en paramètre la liste qui contenait toutes les main des joueurs (qui sont des listes),
+    # j qui est le joueur à qui on doit afficher sa main
+    deck_display = deck_list[j-1] # Ici on récupère la première liste (la prmière main qu'on veut afficher) pour la parcourir 
+    a = 0
+    for i in deck_display: # On parcourt la main (i est un objet)
+        
+        window.blit(i.image,(x + 80*a, y)) # On afficher la carte aux coordonées souhaitées pour la première cartes puis en décalé 
+        a = a + 1
+        
+        
+def afficher_mains_dos_cartes(window):
+    carte_dos = pygame.image.load('Jeu/images_cartes/card_back.png').convert()
+    # pygame. transform. rotate (image, degré)
+    # On affiche les cartes au dos, à gauche 
+    #On ulisite la même structure à gauche, en haut et à droite
+    a = 0
+    carte_dos = pygame. transform. rotate (carte_dos, 90) # On tourne la carte encore de 90° vers la droite 
+    for i in range (11):
+        
+        window.blit(carte_dos,(f.screen_size('x') - 260 , 100 + 40*a)) # On affiche les cartes une part une de la même façon que pour afficher les mains
+        a = a + 1
+    
+    a = 0 
+    carte_dos = pygame. transform. rotate (carte_dos, 90) # On tourne la carte encore de 90° vers la droite   
+    for i in range (11):
+         
+        window.blit(carte_dos,(300 + 80*a , 0 )) # On affiche les cartes une part une de la même façon que pour afficher les mains
+        a = a + 1
+      
+    a = 0 
+    carte_dos = pygame. transform. rotate (carte_dos, 90) # On tourne la carte de 90° vers la droite 
+    for i in range (11):
+        
+        window.blit(carte_dos,(0 , 100 + 40*a)) # On affiche les cartes une part une de la même façon que pour afficher les mains
+        a = a + 1
+    
 
 
 
--- Jeu/main_game.py
+++ Jeu/main_game.py
@@ -11,7 +11,8 @@ pygame.init()
 window = pygame.display.set_mode((f.screen_size('x'),f.screen_size('y')-60))
 pygame.display.set_caption('Vélonimo')
 
-decks_list = c.init_deck()
+liste_des_cartes = c.create_all_cards() # On créé la liste des cartes 
+decks_list = c.init_deck(liste_des_cartes) # On initialise les mains
 
 
 
@@ -23,7 +24,9 @@ while loop :
     for event in pygame.event.get():
         if event.type == QUIT:
             loop = False
-    c.afficher_deck(window,decks_list,1,200,f.screen_size('y')-350)
+    c.afficher_mains_dos_cartes(window)
+    c.afficher_deck(window,decks_list,1,300,f.screen_size('y')-350)
+    
     pygame.display.flip()
     pygame.display.update()
 pygame.quit()
